b028bf9bbe60fe8636cafef53f9f001127bb3786
Florian Mauroner <mauroner.florian@gmail.com>
2021-06-05 16:50:27 +0200

    Project Duplicated - Start

---
+++ Semesterarbeit/ArtworkViews.swift
@@ -0,0 +1,67 @@
+//
+//  ArtworkViews.swift
+//  MapKit_Mojave
+//
+//  Created by macciMac on 04.06.21.
+//  Copyright © 2021 macciMac. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+// replace annotations and pass new color properties
+class ArtworkMarkerView: MKMarkerAnnotationView {
+  override var annotation: MKAnnotation? {
+    willSet {
+      // 1
+      guard let artwork = newValue as? Artwork else {
+        return
+      }
+      canShowCallout = true
+      calloutOffset = CGPoint(x: -5, y: 5)
+      rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+
+      // 2
+      markerTintColor = artwork.markerTintColor
+      glyphImage = artwork.image
+      /*
+      if let letter = artwork.discipline?.first {
+        glyphText = String(letter)
+      }
+      */
+    }
+  }
+}
+
+// using an MKAnnotationView instead of an MKMarkerAnnotationView, and the view has an image property.
+class ArtworkView: MKAnnotationView {
+  override var annotation: MKAnnotation? {
+    willSet {
+      guard let artwork = newValue as? Artwork else {
+        return
+      }
+
+      canShowCallout = true
+      calloutOffset = CGPoint(x: -5, y: 5)
+      //rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+    
+      // Change the right callout accessory info button, so when clicked it shows the Maps icon.
+      let mapsButton = UIButton(frame: CGRect(
+        origin: CGPoint.zero,
+        size: CGSize(width: 48, height: 48)))
+      mapsButton.setBackgroundImage(#imageLiteral(resourceName: "Map"), for: .normal)
+      rightCalloutAccessoryView = mapsButton
+        
+      image = artwork.image
+        
+      // Generate a multi-line label for the detail callout accessory.
+      let detailLabel = UILabel()
+      detailLabel.numberOfLines = 0
+      detailLabel.font = detailLabel.font.withSize(12)
+      detailLabel.text = artwork.subtitle
+      detailCalloutAccessoryView = detailLabel
+
+    }
+  }
+}
+
---
+++ Semesterarbeit/ViewController.swift
@@ -0,0 +1,162 @@
+//
+//  ViewController.swift
+//  MapKit_Mojave
+//
+//  Created by macciMac on 04.06.21.
+//  Copyright © 2021 macciMac. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    // array to hold the Artwork objects from the GeoJSON file
+    private var artworks: [Artwork] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        mapView.centerToLocation(initialLocation)
+        
+        let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
+        let region = MKCoordinateRegion(
+          center: oahuCenter.coordinate,
+          latitudinalMeters: 50000,
+          longitudinalMeters: 60000)
+        mapView.setCameraBoundary(
+          MKMapView.CameraBoundary(coordinateRegion: region),
+          animated: true)
+        
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        mapView.setCameraZoomRange(zoomRange, animated: true)
+        
+        // setting ViewController as the delegate of the map view
+        mapView.delegate = self
+
+        /*
+        // Show artwork on map
+        let artwork = Artwork(
+          title: "King David Kalakaua",
+          locationName: "Waikiki Gateway Park",
+          discipline: "Sculpture",
+          coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        mapView.addAnnotation(artwork)
+        */
+        
+        /*
+        // Here, you register your new class with the map view’s default reuse identifier.
+        // For an app with more annotation types, you would register classes with custom identifiers.
+        mapView.register(
+        ArtworkMarkerView.self,
+        forAnnotationViewWithReuseIdentifier:
+          MKMapViewDefaultAnnotationViewReuseIdentifier)
+        */
+        
+        // Annotations with Images
+        // Here, you register your class "ArtworkView" from ArtworkViews.swift with the map view’s default reuse identifier.
+        mapView.register(
+        ArtworkView.self,
+        forAnnotationViewWithReuseIdentifier:
+          MKMapViewDefaultAnnotationViewReuseIdentifier)
+
+        
+        // add artwork annotations from array to the map
+        loadInitialData()
+        mapView.addAnnotations(artworks)
+        // Do any additional setup after loading the view.
+    }
+    
+    //  read PublicArt.geojson into a Data object and create artwork object using the failable initializer of Artwork
+    private func loadInitialData() {
+      // 1
+      guard
+        let fileName = Bundle.main.url(forResource: "PublicArt", withExtension: "geojson"),
+        let artworkData = try? Data(contentsOf: fileName)
+        else {
+          return
+      }
+
+      do {
+        // 2
+        let features = try MKGeoJSONDecoder()
+          .decode(artworkData)
+          .compactMap { $0 as? MKGeoJSONFeature }
+        // 3
+        let validWorks = features.compactMap(Artwork.init)
+        // 4
+        artworks.append(contentsOf: validWorks)
+      } catch {
+        // 5
+        print("Unexpected error: \(error).")
+      }
+    }
+
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
+
+extension ViewController: MKMapViewDelegate {
+  /*
+  // 1
+  func mapView(
+    _ mapView: MKMapView,
+    viewFor annotation: MKAnnotation
+  ) -> MKAnnotationView? {
+    // 2
+    guard let annotation = annotation as? Artwork else {
+      return nil
+    }
+    // 3
+    let identifier = "artwork"
+    var view: MKMarkerAnnotationView
+    // 4
+    if let dequeuedView = mapView.dequeueReusableAnnotationView(
+      withIdentifier: identifier) as? MKMarkerAnnotationView {
+      dequeuedView.annotation = annotation
+      view = dequeuedView
+    } else {
+      // 5
+      view = MKMarkerAnnotationView(
+        annotation: annotation,
+        reuseIdentifier: identifier)
+      view.canShowCallout = true
+      view.calloutOffset = CGPoint(x: -5, y: 5)
+      view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+    }
+    return view
+  }
+  */
+
+  // tell MapKit what to do when the user taps the callout button
+  func mapView(
+    _ mapView: MKMapView,
+    annotationView view: MKAnnotationView,
+    calloutAccessoryControlTapped control: UIControl
+  ) {
+    guard let artwork = view.annotation as? Artwork else {
+      return
+    }
+
+    let launchOptions = [
+      MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+    ]
+    artwork.mapItem?.openInMaps(launchOptions: launchOptions)
+  }
+
+}
+
